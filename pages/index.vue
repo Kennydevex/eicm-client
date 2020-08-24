6363bbcbfd3a85ab618634fd7713ea3c74581252
Kennydevex <kenny.devex@gmail.com>
2020-08-24 18:31:24 -0100

    Basic Configuration

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="layout">
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  </div>
+</template>
---
+++ plugins/view-design.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import ViewUI from "view-design";
+
+import locale from "view-design/dist/locale/pt-PT";
+Vue.use(ViewUI, { locale });
